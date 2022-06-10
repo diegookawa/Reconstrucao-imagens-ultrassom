f534a1a83a49bbccecab44592dae56e61aac2993
lucadsvenancio <lucasdsvenancio@gmail.com>
2022-06-10 16:09:41 -0300

    convert & requirements

---
+++ convert.py
@@ -0,0 +1,16 @@
+from lib2to3.pytree import convert
+import feather
+import pandas as pd
+from time import time
+
+def convert_feather(path):
+    start_time = time()
+    df = pd.read_csv(f'{path}.csv', header=None)
+    df.columns = df.columns.astype(str)
+    df.to_feather(f'{path}.feather')
+    print(f'\nSize of feather: {len(df)}')
+    end_time = time()
+    return end_time - start_time
+
+if "__main__"  == __name__:
+    convert_feather('Servidor/Models/H-2')
